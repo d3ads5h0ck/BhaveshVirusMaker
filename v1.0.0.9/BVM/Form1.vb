0a7176fb961ae4a997fc7ed13b0fc17a588cd092
Bhavesh_K <bhklyt@gmail.com>
2017-06-28 20:34:08 +0800

    v1.0.0.9 Update
    v1.0.0.9 Update

---
+++ v1.0.0.9/BVM/Form1.vb
@@ -0,0 +1,205 @@
+﻿Public Class Form1
+
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        batchvirusmaker.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Process.Start("https://mega.nz/#!wUEHhSyT!Kw9F2XzWKfdb5WwTfuGECtWuch9ER5NBRn_hC5iUBrY")
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        drag = True
+        mousex = Cursor.Position.X - Me.Left
+        mousey = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mousey
+            Me.Left = Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        bvmLists.wanted = "dorks"
+        bvmLists.Show()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        bvmLists.wanted = "shellcodes"
+        bvmLists.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        bvmLists.wanted = "webshells"
+        bvmLists.Show()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        bvmLists.wanted = "clicodes"
+        bvmLists.Show()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        bvmLists.wanted = "virusgens"
+        bvmLists.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        bvmBrowser.starter = "form"
+        bvmBrowser.WebBrowser1.Navigate("http://www.f2ko.de/en/ob2e.php")
+        bvmBrowser.Show()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Process.Start("http://cyberjokers.pe.hu")
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Process.Start("README.html")
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        bvmBrowser.WebBrowser1.Navigate("http://textfiles.com/magazines/")
+        bvmBrowser.starter = "form"
+        bvmBrowser.Show()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Try
+            Process.Start("VirusScreen.exe")
+        Catch ex As Exception
+            Process.Start("http://cyberjokers.pe.hu/bvm/downloads.html")
+        End Try
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Try
+            Process.Start("pyCipher.py")
+        Catch ex As Exception
+            Process.Start("http://cyberjokers.pe.hu/bvm/downloads.html")
+        End Try
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Try
+            Process.Start("pcscanner.exe")
+        Catch ex As Exception
+            Process.Start("http://cyberjokers.pe.hu/bvm/downloads.html")
+        End Try
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Try
+            Process.Start("rarcracker.exe")
+        Catch ex As Exception
+            Process.Start("http://cyberjokers.pe.hu/bvm/downloads.html")
+        End Try
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Process.Start("http://cyberjokers.pe.hu/bvm/downloads.html")
+    End Sub
+
+    Private Sub Form1_DoubleClick(sender As Object, e As EventArgs) Handles MyBase.DoubleClick
+        If Me.WindowState = FormWindowState.Normal Then
+            Me.WindowState = FormWindowState.Maximized
+        Else
+            Me.WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        bvmBrowser.WebBrowser1.Navigate("https://sourceforge.net/projects/bhavesh-virus-maker-skw/files/")
+        bvmBrowser.starter = "form"
+        bvmBrowser.Show()
+        MsgBox("See if you have the latest version..")
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Process.Start("https://github.com/NyanSniper101/BhaveshVirusMaker")
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        Try
+            Process.Start("Bhavesh Shellsploit Framework\Bhavesh Shellsploit Framework\bin\Debug\Bhavesh Shellsploit Framework.exe")
+        Catch ex As Exception
+            Process.Start("http://cyberjokers.pe.hu/bvm/downloads.html")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Settings.firsttime = 0 'for testing only
+        btndesigner()
+        If My.Settings.firsttime = 0 Then
+            Process.Start("README.html")
+            My.Settings.firsttime = 1
+        Else
+            'nothing
+        End If
+    End Sub
+
+    Sub btndesigner()
+        Dim p As New Drawing2D.GraphicsPath
+        Dim q As New Drawing2D.GraphicsPath
+        Dim r As New Drawing2D.GraphicsPath
+
+        'Button 16
+        p.StartFigure()
+        p.AddArc(New Rectangle(0, 0, 10, 10), 180, 90)
+        p.AddLine(10, 0, Button16.Width - 10, 0)
+        p.AddArc(New Rectangle(Button16.Width - 40, 0, 40, 40), -90, 180)
+        p.AddLine(Button16.Width, 40, Button16.Width, Button16.Height - 40)
+        p.AddArc(New Rectangle(Button16.Width - 1, Button16.Height - 1, 1, 1), 0, 180)
+        p.AddLine(Button16.Width - 40, Button16.Height, 40, Button16.Height)
+        p.AddArc(New Rectangle(0, Button16.Height - 10, 10, 10), 180, 90)
+        p.CloseFigure()
+        Button16.Region = New Region(p)
+
+        'Button 17
+        q.StartFigure()
+        q.AddArc(New Rectangle(0, 0, 10, 10), 180, 90)
+        q.AddLine(10, 0, Button17.Width - 10, 0)
+        q.AddArc(New Rectangle(Button17.Width - 40, 0, 40, 40), -90, 180)
+        q.AddLine(Button17.Width, 40, Button17.Width, Button17.Height - 40)
+        q.AddArc(New Rectangle(Button17.Width - 1, Button17.Height - 1, 1, 1), 0, 180)
+        q.AddLine(Button17.Width - 40, Button17.Height, 40, Button17.Height)
+        q.AddArc(New Rectangle(0, Button17.Height - 10, 10, 10), 180, 90)
+        q.CloseFigure()
+        Button17.Region = New Region(q)
+
+        'Button 18
+        r.StartFigure()
+        r.AddArc(New Rectangle(0, 0, 10, 10), 180, 90)
+        r.AddLine(10, 0, Button18.Width - 10, 0)
+        r.AddArc(New Rectangle(Button18.Width - 40, 0, 40, 40), -90, 180)
+        r.AddLine(Button18.Width, 40, Button18.Width, Button18.Height - 40)
+        r.AddArc(New Rectangle(Button18.Width - 1, Button18.Height - 1, 1, 1), 0, 180)
+        r.AddLine(Button18.Width - 40, Button18.Height, 40, Button18.Height)
+        r.AddArc(New Rectangle(0, Button18.Height - 10, 10, 10), 180, 90)
+        r.CloseFigure()
+        Button18.Region = New Region(r)
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        Try
+            Process.Start("MalwareOrganizer.exe")
+        Catch ex As Exception
+            Process.Start("http://cyberjokers.pe.hu/bvm/downloads.html")
+        End Try
+    End Sub
+End Class
---
+++ v1.0.0.9/BVM/bvmLists.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class bvmLists
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(bvmLists))
+        Me.catcom = New System.Windows.Forms.ComboBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'catcom
+        '
+        Me.catcom.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.catcom.BackColor = System.Drawing.Color.Black
+        Me.catcom.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.catcom.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.catcom.ForeColor = System.Drawing.Color.Lime
+        Me.catcom.FormattingEnabled = True
+        Me.catcom.Location = New System.Drawing.Point(141, 55)
+        Me.catcom.Name = "catcom"
+        Me.catcom.Size = New System.Drawing.Size(507, 24)
+        Me.catcom.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(742, 36)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(692, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 36)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Selector"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Choose Category"
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Gray
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(668, 51)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(62, 31)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "OK"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.Color.Lime
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 98)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(718, 336)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Black
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red
+        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(642, 0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(50, 36)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "[ ]"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Black
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button4.FlatAppearance.BorderSize = 0
+        Me.Button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red
+        Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(592, 0)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(50, 36)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "_"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.Lime
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 98)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(718, 336)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.Visible = False
+        '
+        'bvmLists
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(742, 454)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.catcom)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "bvmLists"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "BVM : SELECTOR"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents catcom As ComboBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
